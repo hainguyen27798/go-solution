9f9ab01233ea2f84be1294845645c84b6605c494
harry Nguyen <hainguyen27798@gmail.com>
2025-02-17 19:36:31 +0700

    pipeline

--- examples/pipe_line.go
+++ examples/pipe_line.go
@@ -1,12 +1,47 @@
 package examples
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func Pow(muns []int) <-chan int {
+// isPrime returns true if n is a prime number.
+func isPrime(n int) bool {
+	// Handle numbers less than 2 (0 and 1 are not prime)
+	if n < 2 {
+		return false
+	}
+
+	// Check for divisors from 2 up to the square root of n.
+	// If any divisor is found, n is not prime.
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func primeFilter(muns []int) <-chan int {
 	out := make(chan int)
 
 	go func() {
-		for _, m := range muns {
+		for _, mun := range muns {
+			if isPrime(mun) {
+				out <- mun
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func Pow(muns <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for m := range muns {
 			out <- m * m
 		}
 		close(out)
@@ -16,5 +51,13 @@ func Pow(muns []int) <-chan int {
 }
 
 func RunPipeLineExample() {
-	fmt.Println(Pow([]int{1, 2, 3, 4, 5}))
+	nums := []int{1, 2, 3, 4, 5}
+
+	primeNums := primeFilter(nums)
+
+	rs := Pow(primeNums)
+
+	for num := range rs {
+		fmt.Println(num)
+	}
 }
